0dab4ce7238770880f60181aded68a65cf9cd26a
Pepe Dietrich <law@Mac.fritz.box>
2026-05-26 17:35:24 +0200

    fix vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -9,3 +9,15 @@ const config = {
 };
 
 export default config;
+
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const config = {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter()
+  }
+};
+
+export default config;
